516a6412c0b91b3c95ee3029489975f71248aacd
Gabriel Carvalho <80786139+GabPhoenix@users.noreply.github.com>
2021-12-07 21:22:55 -0300

    Gabriel Carvalho

--- exprova2.asm
+++ exprova2.asm
@@ -1,57 +1,57 @@
-# Escreva um programa em Assembly MIPS que lê dois números e verifica 
+# Escreva um programa em Assembly MIPS que lÃª dois nÃºmeros e verifica 
 # qual o maior deles. Submeta um arquivo do MARS .asm ou um aquivo .txt 
 
 .data	
 	msg_1: .asciiz "Informe um numero: "
 	msg_2: .asciiz "Informe outro numero: "
-	msg_3: .asciiz "Os valores informados são iguais"
+	msg_3: .asciiz "Os valores informados sÃ£o iguais"
 	msg_4: .asciiz "O maior numero e: "
 .text 
-	li $v0, 4 # Comando de impressão de String 
-	la $a0, msg_1 # Passando o enderaço da primeira mensagem
+	li $v0, 4 # Comando de impressÃ£o de String 
+	la $a0, msg_1 # Passando o enderaÃ§o da primeira mensagem
 	syscall 
 	
-	# recebendo o primero número 
+	# recebendo o primero nÃºmero 
 	li $v0, 5 # Comando para receber um inteiro 
 	syscall 
 	
 	move $t0, $v0 # Movendo o valor informado para o registrador $t0
 	
-	li $v0, 4 # Comando de impressão de String 
-	la $a0, msg_2 # Passando o enderaço da segunda mensagem
+	li $v0, 4 # Comando de impressÃ£o de String 
+	la $a0, msg_2 # Passando o enderaÃ§o da segunda mensagem
 	syscall 
 	
-	# recebendo o segundo número 
+	# recebendo o segundo nÃºmero 
 	li $v0, 5 # Comando para receber um inteiro 
 	syscall 
 	
 	move $t1, $v0 # Movendo o valor informado para o registrador $t1
 	
-	beq $t0, $t1 iguais # Os valores são iguais
+	beq $t0, $t1 iguais # Os valores sÃ£o iguais
 	bgt $t0, $t1 maior1 # imprimindo o maior
 	bgt $t1, $t0 maior2 # imprimindo o maior
 	
-	iguais: li $v0, 4 #Instução para imprimir String 
-	la $a0, msg_3 # Passando o endereço da mensagem 
+	iguais: li $v0, 4 #InstuÃ§Ã£o para imprimir String 
+	la $a0, msg_3 # Passando o endereÃ§o da mensagem 
 	syscall #run
-	li $v0, 10 # Comando para encerrar o programar (para não ir para o próximo método)
+	li $v0, 10 # Comando para encerrar o programar (para nÃ£o ir para o prÃ³ximo mÃ©todo)
 	syscall 
 	
-	maior1: li $v0, 4 #Instução para imprimir String 
-	la $a0, msg_4 # Passando o endereço da mensagem 
+	maior1: li $v0, 4 #InstuÃ§Ã£o para imprimir String 
+	la $a0, msg_4 # Passando o endereÃ§o da mensagem 
 	syscall #run
-	li $v0, 1 #Instução para imprimir um inteiro 
+	li $v0, 1 #InstuÃ§Ã£o para imprimir um inteiro 
 	move $a0, $t0 # movendo o valor
 	syscall #run
-	li $v0, 10 # Comando para encerrar o programar (para não ir para o próximo método)
+	li $v0, 10 # Comando para encerrar o programar (para nÃ£o ir para o prÃ³ximo mÃ©todo)
 	syscall # run
 	
-	maior2: li $v0, 4 #Instução para imprimir String 
-	la $a0, msg_4 # Passando o endereço da mensagem 
+	maior2: li $v0, 4 #InstuÃ§Ã£o para imprimir String 
+	la $a0, msg_4 # Passando o endereÃ§o da mensagem 
 	syscall #run
-	li $v0, 1 #Instução para imprimir um inteiro 
-	move $a0, $t1 # passando o endereço
+	li $v0, 1 #InstuÃ§Ã£o para imprimir um inteiro 
+	move $a0, $t1 # movendo o valor
 	syscall #run
-	li $v0, 10 # Comando para encerrar o programar (para não ir para o próximo método)
+	li $v0, 10 # Comando para encerrar o programar 
 	syscall # run
 	
